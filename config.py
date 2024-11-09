78c59e1e9bb95946611837822f77820a088ad7e8
isaias <isaiasgarraeluta@gmail.com>
2024-11-09 13:33:17 -0300

    feat:

--- config.py
+++ config.py
@@ -1,7 +1,8 @@
 import json
+import os
 def getConfig():
     
     print("\nload config...\n")
-    with open('./data/config.json', 'r') as file:
+    with open(os.path.abspath('./data/config.json'), 'r') as file:
         
         return json.load(file)
