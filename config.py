b1196dd6ab1e79d77533cee1005b9052430b71f2
isaias-silva <isaiasgarraeluta@gmail.com>
2024-11-16 12:27:10 -0300

    feat: gitignore

--- config.py
+++ config.py
@@ -3,6 +3,6 @@ import os
 def getConfig():
     
     print("\nload config...\n")
-    with open(os.path.abspath('./data/config.json'), 'r') as file:
+    with open(os.path.abspath('data/config.json'), 'r') as file:
         
         return json.load(file)
